fa494d852ea59edbcd272435b4ec8236e5bce7bd
keht-agta <keht-agta@yandex.ru>
2024-07-27 17:39:45 +0800

    1
    dz smart home

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from measurement.views import ListCreateSensorsView, \
+    ListUpdateSensorView, MeasurementCreateSerializerView
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', ListCreateSensorsView.as_view()),
+    path('sensors/<pk>/', ListUpdateSensorView.as_view()),
+    path('measurements/', MeasurementCreateSerializerView.as_view()),
 ]
